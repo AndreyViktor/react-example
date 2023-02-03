8f2ca08ca6cc33151cadbc05c14526f3e561e223
andreyviktor <andrey.viktor@5by5.com.br>
2023-02-03 19:50:24 -0300

    create react base example app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter,
+  Route
+} from "react-router-dom";
+import Splash from "./components/Splash.js"
+import Home from "./components/Home.js"
+import Search from "./components/Search.js"
+import Buttons from "./components/Buttons.js"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Route component = { Splash }  path="/splash" exact />
+        <Route component = { Home }  path="/home" />
+        <Route component = { Search }  path="/search" />
+        <Route component = { Buttons }  path="/buttons" />
+    </BrowserRouter>
     </div>
   );
 }
