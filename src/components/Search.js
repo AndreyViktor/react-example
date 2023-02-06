dfec768b58dd0f966d18b1e6a14158cfb92608ac
andreyviktor <andrey.viktor@5by5.com.br>
2023-02-06 00:39:41 -0300

    add search screen

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,43 @@
-import React from "react";
-import {
-  BrowserRouter,
-  Route
-} from "react-router-dom";
-import Home from "./Home.js"
+import React, {useState, useEffect} from "react";
+import { useNavigate } from "react-router-dom";
+import GetStrings from "../utils/GetStringsUseCase"
 
-function Search() {
+function Search({ setSelectedString }) {
+    const navigate = useNavigate()
+    const [searchTerm, setSearchTerm] = useState("");
+    const [strings, setStrings] = useState([""]);
+    const [filteredStrings, setFilteredStrings] = useState(strings);
+  
+    useEffect(() => {
+      setFilteredStrings(
+        strings.filter(string =>
+          string.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+      );
+    }, [searchTerm, strings]);
+
+    useEffect(()=>{
+        let stringList = GetStrings
+        setStrings(stringList)
+    })
+  
+    const handleSelectString = string => {
+      setSelectedString(string);
+      navigate("/home")
+    };
+
   return (
     <div>
-
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={e => setSearchTerm(e.target.value)}
+      />
+      {filteredStrings.map(string => (
+        <p key={string} onClick={() => handleSelectString(string)}>
+          {string}
+        </p>
+      ))}
     </div>
   );
 }
---
+++ src/utils/GetStringsUseCase.js
@@ -0,0 +1,3 @@
+export default function GetStrings(){
+    return ["Abacate","Melancia", "Picanha", "Mamão", "Arroz","Feijão"]
+}
