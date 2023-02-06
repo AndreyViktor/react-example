4cc3c86264667dddec1c92b1116498eeb3470c3e
andreyviktor <andrey.viktor@5by5.com.br>
2023-02-06 00:40:29 -0300

    add time out effect to splash screen

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,18 +1,21 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
+import { useNavigate } from "react-router-dom";
 import "./splash.css"
 
-import {
-  BrowserRouter,
-  Route
-} from "react-router-dom";
 import Home from "./Home.js"
 import Logo from "../Logo.jpeg"
 
 function Splash() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    setTimeout(() => navigate("/home"), 5000);
+  }, []);
+
   return (
     <div>
-      <img className="Logo" src={Logo} alt="" />
-      <p className="p">The bigger the train, the greater the impact.</p>
+      <img src={Logo} alt="" />
+      <p className="text">The bigger the train, the greater the impact.</p>
     </div>
   );
 }
