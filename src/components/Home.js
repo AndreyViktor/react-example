97562fe2a87c8dcb3df9f477f44dd166ff2217eb
andreyviktor <andrey.viktor@5by5.com.br>
2023-02-06 20:37:11 -0300

    refactor

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ function Home({ selectedString }) {
       <Link className="Link" to="/search">tela de busca</Link>
       <Link className="Link" to="/counter">Contador</Link>
       {
-        selectedString != "" ?
+        selectedString !== "" ?
         <p className="ResultText">{selectedString}</p> : <div/>
       }
       
